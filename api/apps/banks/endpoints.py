e2c937e840fd761a147f672a0d1050d872bf65c6
IlyaBulatau <ilyabulatau@gmail.com>
2024-02-03 13:15:19 +0200

    mod enpoints banks

--- api/apps/banks/endpoints.py
+++ api/apps/banks/endpoints.py
@@ -5,14 +5,15 @@ from typing import List
 
 from api.apps.constants import BankEnum, CurrencyEnum
 from api.apps.banks.schemes import BankScheme, CurrencyScheme
-from api.services.main import get_national_bank_currencies_list
+from api.services.nb import get_national_bank_currencies_list
+from api.services.alfa import get_alpha_bank_currency_list
 
 router = APIRouter(prefix="/banks", tags=["Bank"])
 
 
 banks_methods = {
-    BankEnum.NB.value: get_national_bank_currencies_list,
-    BankEnum.ALFA.value: get_national_bank_currencies_list,
+    BankEnum.NB.name: get_national_bank_currencies_list,
+    BankEnum.ALFA.name: get_alpha_bank_currency_list,
 }
 
 
@@ -34,11 +35,12 @@ async def get_banks():
         response_model=List[CurrencyScheme],
         summary="Валюты по банку"
         )
-async def detail_bank(bank_name: str):
-    bank = [bank.value for bank in BankEnum if bank.value == bank_name]
+async def detail_bank(bank_name: BankEnum):
+    bank = [bank.value for bank in BankEnum if bank.value == bank_name.value]
     if not bank:
         return JSONResponse(content={"Status": "Error", "Msg": "Bank doesn't exists"}, status_code=404)
-    implemet = banks_methods.get(BankEnum.NB.value)
+    implemet = banks_methods.get(bank_name.name)
     currencies_list: List[str] = await implemet()
     currensie_set = set(currencies_list).intersection(set([bank.name for bank in CurrencyEnum]))
+    print(currencies_list, currensie_set)
     return [CurrencyScheme(name=currency) for currency in currensie_set]

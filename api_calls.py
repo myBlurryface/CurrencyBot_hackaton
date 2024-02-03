131f4c8fd08d4593310c9773414ede421097a7b0
Blurryface <ralf_201@hotmail.com>
2024-02-03 16:46:37 +0300

    Merge with apis

--- api_calls.py
+++ api_calls.py
@@ -1,6 +1,7 @@
 import aiohttp
 from constants import BANK_SHOW
 from api.apps.constants import CurrencyEnum
+import dicts as DCT
 
 
 banks_dict = {
@@ -9,13 +10,17 @@ banks_dict = {
     "Беларусь Банк": "belbank"
 }
 
-async def get_currency():
-    currency_list = []
-    
-    return currency_list
+async def get_currency(bank):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"http://localhost:8000/api/v1/banks/{banks_dict[bank]}/currencies") as response:
+            result = await response.json()
+
+    DCT.currency = [item.get("name") for item in result]
+    return DCT.currency
 
 async def get_banks_list():
     async with aiohttp.ClientSession() as session:
         async with session.get("http://localhost:8000/api/v1/banks") as response:
             result = await response.json()
-    return [(BANK_SHOW.get(r.get("name")), banks_dict.get(BANK_SHOW.get(r.get("name")))) for r in result]
+    DCT.banks = [BANK_SHOW.get(r.get("name")) for r in result]
+    return DCT.banks

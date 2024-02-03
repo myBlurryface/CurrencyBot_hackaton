0ba6f0c03640854ab04f42038744eb11fe250a96
Anton Sivashka <anton.sivashko@gmail.com>
2024-02-03 16:22:25 +0300

    Added rate delta graph

--- api/services/nb.py
+++ api/services/nb.py
@@ -1,8 +1,10 @@
 import aiohttp
+import matplotlib.pyplot as plt
 
 import asyncio
 import json
-import datetime
+import calendar
+from datetime import date
 
 
 USD_LINK = "https://api.nbrb.by/exrates/rates/USD?parammode=2"
@@ -63,12 +65,38 @@ async def get_national_bank_currency_by_date(currency_name: str, date=""):
     return None
 
 
-async def get_national_bank_currency_delta(currency: str):
-    async with aiohttp.ClientSession() as session:
-        url = "https://api.nbrb.by/exrates/rates/USD?parammode=2"
-        async with session.get(url) as response:
-            if response.status == 200:
-                data = await response.json()
-                print(data)
-            else:
-                print(f"Ошибка запроса: {response.status}")
+async def get_national_bank_currency_delta(currency_name: str):
+    today = date.today()
+    year = today.year
+    month = today.month
+    if month == 1:
+        month = 12
+        year -= 1
+    else:
+        month -= 1
+    dates = calendar.Calendar().itermonthdates(year, month)
+    courses = dict()
+    for months_date in dates:
+        async with aiohttp.ClientSession() as session:
+            url = f"https://api.nbrb.by/exrates/rates?ondate={months_date}&periodicity=0"
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    for currency in data:
+                        if currency["Cur_Abbreviation"] == currency_name:
+                            courses[f"{months_date}"] = currency["Cur_OfficialRate"]
+                else:
+                    print(f"Ошибка запроса: {response.status}")
+    build_graph_by_day(courses)
+    return courses
+
+
+def build_graph_by_day(currency_cource: dict):
+    dates = list(currency_cource.keys())
+    rates = list(currency_cource.values())
+    
+    plt.plot(dates, rates)
+    plt.xlabel("Date")
+    plt.ylabel("Rate")
+    plt.title("Изменение курса валюты")
+    plt.savefig("nb_delta.png")
